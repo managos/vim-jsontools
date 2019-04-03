195b764f71b558da0eff923c6a3661469173cdb4
Manago Shotaro <manago@colon.ne.jp>
2019-04-03 21:25:17 +0900

    initial commit

---
+++ rplugin/python3/vimjsontools/__init__.py
@@ -0,0 +1,36 @@
+import json
+import pynvim
+
+
+@pynvim.plugin
+class JsonToolsPlugin(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.function('JsonToolsPrettify', sync=True)
+    def prettify(self, args):
+        '''jsonを整形するコマンド'''
+        jsontext = self._get_text()
+        d = self._to_dict(jsontext)
+        self.nvim.current.buffer[:] = self._to_text(d).split('\n')
+
+    @pynvim.function('JsonToolsCheckValidateFormat', sync=True)
+    def validate(self, args):
+        try:
+            d = json.loads(self._get_text())
+            self.nvim.out_write('json ok\n')
+        except Exception as e:
+            self.nvim.err_write('{}\n'.format(str(e)))
+
+    def _get_text(self):
+        return '\n'.join(self.nvim.current.buffer[:])
+
+    def _to_dict(self, text):
+        return json.loads(text)
+
+    def _to_text(self, d):
+        return json.dumps(
+            d,
+            ensure_ascii=False,
+            indent=2
+        )
---
+++ tests/__init__.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+from pynvim import attach
+from rplugin.python3.vimjsontools import JsonToolsPlugin
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+
+class TestSomething(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.nvim = attach('socket', path='/tmp/nvim')
+        self.nvim.command('e {}/tests/data.json'.format(BASE_DIR))
+        self.plugin = JsonToolsPlugin(self.nvim)
+
+    def test_get_text(self):
+        self.assertEqual(
+            self.plugin._get_text(),
+            '''{"key": "value"
+}'''
+        )
+
+    def test_to_dict(self):
+        text = self.plugin._get_text()
+        d = self.plugin._to_dict(text)
+        self.assertEqual(d['key'], 'value')
+
+    def test_validate(self):
+        self.plugin.validate(None)
+
+    def test_prettify(self):
+        self.plugin.prettify(None)
